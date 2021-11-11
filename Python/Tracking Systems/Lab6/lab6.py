5b1dd5c48c314af18611045d763808fec5fb0161
Brandon Shumin <47755904+bshumin@users.noreply.github.com>
2021-11-11 00:06:04 -0500

    Update lab6.py

--- Python/Tracking Systems/Lab6/lab6.py
+++ Python/Tracking Systems/Lab6/lab6.py
@@ -37,59 +37,6 @@ def readData(file_in):
         print('Something went wrong in parsing the data from ' + file_in)
 
 
-# create an extended kalman filter of model type sin/10
-def extended_kalman_sin10(Q, R, T, pos, true_pos, title):
-
-    dfda = np.mat([[0, 0, 0],
-                   [0, 1, 0],
-                   [0, 0, 0]])
-    dgdx = np.mat([[0, 0, 1]])
-    dgdn = np.mat([1])
-
-    St = np.mat([[1, 0, 0],
-                 [0, 1, 0],
-                 [0, 0, 1]])
-    Xt = np.mat([0, 0, 0]).getT()
-    pred = []
-
-    Q = np.mat([[0, 0, 0],
-                [0, Q, 0],
-                [0, 0, 0]])
-    R = np.mat([R])
-
-    for val in range(len(pos)):
-        fxtm1tm1 = np.mat([[Xt[0, 0] + Xt[1, 0] * T], [Xt[1, 0]], [sin(Xt[0, 0] / 10)]])
-        # predict next state
-        Xt = fxtm1tm1
-        gxtnt = [Xt[2, 0]]
-        dfdx = np.mat([[1, T, 0],
-                       [0, 1, 0],
-                       [(1 / 10) * cos(Xt[0, 0] / 10), 0, 0]])
-        # predict next state covariance
-        St = dfdx*St*dfdx.getT() + dfda*Q*dfda.getT()
-        # Obtain measurement
-        Yt = pos[val]
-        # calculate Kalman gain
-        Kt = St*dgdx.getT()*pow((dgdx*St*dgdx.getT() + dgdn*R*dgdn.getT()), -1)
-        # update state
-        Xt = Xt + Kt*(Yt - gxtnt[0])
-        # update state covariance
-        St = (np.identity(3)-Kt*dgdx)*St
-        pred.append(Xt[2][0])
-
-    pred = np.squeeze(np.asarray(pred))
-    print(pred)
-
-    plt.plot(np.linspace(0, T * len(pos), len(pos)), pred, 'black', linestyle="-")
-    plt.plot(np.linspace(0, T * len(pos), len(pos)), true_pos, 'grey', linestyle=":")
-    # plt.scatter(x=np.linspace(0, T * len(pos), len(pos)), y=pos, marker='.')
-    plt.xlabel('Time')
-    plt.ylabel('Position')
-    plt.title(title)
-    plt.show()
-    plt.close()
-
-
 def particle_filter(pos, vel, meas, T=1, M=300):
     # instantiate Xm and Wm matrices
     Xm = []
