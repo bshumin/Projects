df28306484f01a2d27e88cc9fc022dcef5bc71b1
bshumin <bshumin@clemson.edu>
2021-11-11 19:17:43 -0500

    Update lab6.py

--- Python/Tracking Systems/Lab6/lab6.py
+++ Python/Tracking Systems/Lab6/lab6.py
@@ -37,17 +37,18 @@ def readData(file_in):
         print('Something went wrong in parsing the data from ' + file_in)
 
 
-def particle_filter(pos, vel, meas, T=1, M=300):
+def particle_filter(pos, vel, meas, T=1, M=300, ESS_Thresh=0.5):
     # instantiate Xm and Wm matrices
     Xm = []
     W = []
     for i in range(M):
         Xm.append([[0], [0]])
-        W.append(1/(i+1))
+        W.append(1/(M))
     Xm = np.array(Xm)
     W = np.mat(W)
+    print(Xm)
 
-    siga = 0.0625
+    siga = 0.25
     sigm = 4.0
     sign = 0.003906
     xm1 = -10
@@ -63,18 +64,19 @@ def particle_filter(pos, vel, meas, T=1, M=300):
         sum_W = 0
         new_Xm = []
         for i in range(M):
+            print(Xm[i - 1, 0, 0])
             if Xm[i, 0, 0] < -20:
                 fx = np.mat([[Xm[i, 0, 0] + Xm[i, 1, 0] * T], [2]])
             elif (-20 <= Xm[i, 0, 0]) and (Xm[i, 0, 0] < 0):
-                fx = np.mat([[Xm[i, 0, 0] + Xm[i, 1, 0] * T], [Xm[i, 1, 0] + abs(rand.gauss(0, siga))]])
+                fx = np.mat([[Xm[i, 0, 0] + Xm[i, 1, 0] * T], [Xm[i, 1, 0] + abs(rand.gauss(0, pow(siga, 2)))]])
             elif (0 <= Xm[i, 0, 0]) and (Xm[i, 0, 0] <= 20):
-                fx = np.mat([[Xm[i, 0, 0] + Xm[i, 1, 0] * T], [Xm[i, 1, 0] - abs(rand.gauss(0, siga))]])
+                fx = np.mat([[Xm[i, 0, 0] + Xm[i, 1, 0] * T], [Xm[i, 1, 0] - abs(rand.gauss(0, pow(siga, 2)))]])
             else:
                 fx = np.mat([[Xm[i, 0, 0] + Xm[i, 1, 0] * T], [-2]])
 
             gx = (1 / (sqrt(2 * pi) * sigm)) * exp((-pow(Xm[i, 0, 0] - xm1, 2)) / (2 * pow(sigm, 2))) + \
                  (1 / (sqrt(2 * pi) * sigm)) * exp((-pow(Xm[i, 0, 0] - xm2, 2)) / (2 * pow(sigm, 2)))
-            py = np.mat((1 / (sqrt(2 * pi) * sign)) * exp((-pow(gx - pos[i], 2)) / (2 * pow(sign, 2))))
+            py = np.mat((1 / (sqrt(2 * pi) * sign)) * exp((-pow(gx - meas[i], 2)) / (2 * pow(sign, 2))))
             W[0, i] = W[0, i]*py[0, 0]
             sum_W += W[0, i]
             new_Xm.append(fx)
@@ -83,12 +85,46 @@ def particle_filter(pos, vel, meas, T=1, M=300):
 
         sum_pos = 0
         sum_vel = 0
+        CV = 0
         for i in range(M):
             W[0, i] = W[0, i]/sum_W  # normalize weights
             sum_pos += W[0, i] * Xm[i, 0, 0]
             sum_vel += W[0, i] * Xm[i, 1, 0]
+            CV += pow(M * W[0, i] - 1, 2)
+
+        CV = (1/M) * CV
+        ESS = M / (1 + CV) - 1
+        # print(ESS)
+
         pos_pred.append(sum_pos)
         vel_pred.append(sum_vel)
+
+        # resample here
+        if ESS < 0.1:
+            Q = np.cumsum(W)
+            t = np.random.uniform(0, 1, M)
+            T = np.sort(t)
+            #T[M+1] = 1.0  # set boundary condition
+            i = j = 1
+            Index = [0] * M
+            while i < M:
+                if T[i] < Q[0, j]:
+                    Index[i] = j
+                    i += 1
+                else:
+                    j += 1
+            i = 0
+            new_Xm = []
+            new_W = []
+            while i < M:
+                new_Xm.append([[Xm[Index[i], 0, 0]], [Xm[Index[i], 1, 0]]])
+                print([[Xm[Index[i], 0, 0]], [Xm[Index[i], 1, 0]]])
+                new_W.append(1/M)
+                i += 1
+            Xm = np.array(new_Xm)
+            W = new_W
+            W = np.mat(W)
+            print(Xm[i-1, 0, 0])
 
     pos_pred = np.squeeze(np.asarray(pos_pred))
     vel_pred = np.squeeze(np.asarray(vel_pred))
